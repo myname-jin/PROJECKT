ffd0c61a9c2825ae788eee922f10aed3d7586f4d
myname-jin <adsd336@daum.net>
2025-03-26 23:21:35 +0900

    as
    as

--- src/main/java/com/mycompany/javaprojeckt/JAVAPROJECKT.java
+++ src/main/java/com/mycompany/javaprojeckt/JAVAPROJECKT.java
@@ -11,6 +11,6 @@ package com.mycompany.javaprojeckt;
 public class JAVAPROJECKT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hellso !");
     }
 }
