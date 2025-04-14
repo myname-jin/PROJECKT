fcc0fe1bb819927bf6117870933d689cd91f8ee2
myname-jin <adsd336@daum.net>
2025-04-14 12:55:16 +0900

    test
    file test code

--- src/main/java/management/NoticeController.java
+++ src/main/java/management/NoticeController.java
@@ -16,12 +16,11 @@ public class NoticeController {
     public NoticeController(NoticeModel model) {
         this.model = model;
     }
-
-    // 공지사항 저장
+// 공지사항 저장
     public void saveNotice(String noticeContent) {
         model.saveNotice(noticeContent);  // 파일에 저장
     }
-
+   
     // 공지사항 목록을 읽어서 반환
     public String[] loadNotices() {
         return model.loadNotices();  // 파일에서 공지사항 읽어오기
