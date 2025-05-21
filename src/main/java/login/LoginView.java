16b786acaa881d974f7118e5849ea6e594a31380
seongjin111 <msj0228a@naver.com>
2025-05-22 02:20:44 +0900

    Feat: 비밀번호 찾기/변경 버튼 생성
    LoginView만 수정해서 로그인 화면에 버튼만 생성함.
    아직 기능 구현은 안 되는 상태임.

--- src/main/java/login/LoginView.java
+++ src/main/java/login/LoginView.java
@@ -16,21 +16,23 @@ public class LoginView extends JFrame {
     public JButton btnLogin = new JButton("로그인");
     public JRadioButton userRadio = new JRadioButton("사용자");
     public JRadioButton adminRadio = new JRadioButton("관리자");
-    public JButton btnSignup = new JButton("회원가입"); // 회원가입 버튼 생성
+    public JButton btnSignup = new JButton("회원가입");
+    public JButton btnPw = new JButton("비밀번호 찾기/변경 "); // 비밀번호 찾기/변경 버튼 생성
 
     public LoginView() {
         super("로그인 화면");
-        setSize(350, 250);
+        setSize(350, 270);
         setLayout(null);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         txtUser.setBounds(80, 30, 180, 30);
         txtPass.setBounds(80, 70, 180, 30);
-        btnLogin.setBounds(80, 160, 80, 30); // 로그인 버튼 위치 수정(130 -> 80)
+        btnLogin.setBounds(80, 145, 80, 30); // 로그인 버튼 위치 수정(130 -> 80)
         userRadio.setBounds(80, 110, 80, 20);
         adminRadio.setBounds(160, 110, 80, 20);
-        btnSignup.setBounds(170, 160, 85, 30); // 회원가입 버튼 위치 지정
+        btnSignup.setBounds(170, 145, 85, 30); // 회원가입 버튼 위치 지정
+        btnPw.setBounds(80, 185, 175, 30);
 
         ButtonGroup group = new ButtonGroup();
         group.add(userRadio);
@@ -42,7 +44,8 @@ public class LoginView extends JFrame {
         add(btnLogin);
         add(userRadio);
         add(adminRadio);
-        add(btnSignup); // 회원가입 버튼 추가
+        add(btnSignup);
+        add(btnPw); // 비밀번호 찾기/변경 버튼 추가
     }
 
     public String getUserId() {
