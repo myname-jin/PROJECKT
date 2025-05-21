c0f7c7ec72a247b6a7bb1a52c83ed4cfd46291ff
seongjin111 <msj0228a@naver.com>
2025-05-22 01:56:24 +0900

    Refactor: 회원가입 정보 전달코드 수정 및 빈줄 제거, Comment: 주석 변경
    1. 회원가입 txt에 append모드가 아니라 덮어쓰도록 코드를 수정해서
    로그인txt에 회원가입 정보가 중복저장되지 않도록 함.
    2. 회원가입 정보가 role에 해당하는 로그인 파일로만 전달하도록 수정함.

--- src/main/java/login/SignupController.java
+++ src/main/java/login/SignupController.java
@@ -28,32 +28,33 @@ public class SignupController {
         String userName = view.getName();   // userName 변수에 회원가입 화면의 이름 입력값을 저장
         String userDept = view.getDept();   // userDept 변수에 회원가입 화면의 학과 입력값을 저장
         
-
         if (userId.isEmpty() || password.isEmpty()) {
             JOptionPane.showMessageDialog(view, "아이디와 비밀번호를 입력하세요.");
             return;
-        }
+        }        
         
-        
-
         boolean success = model.registerUser(userId, password, role, userName, userDept);
 
         if (success) {
-            // 새로 회원가입한 정보롤 로그인 파일로 전달하는 메서드 호출
-            model.adminTransfer();
-            model.userTransfer();
+            // 새로 회원가입한 정보롤 role에 해당하는 로그인 파일에만 전달하도록 수정
+            if (role.equals("admin")){
+                model.adminTransfer();
+            } else {
+                model.userTransfer();
+            }
             // 모든 로그인 정보를 예약에 필요한 텍스트 파일로 전달하는 메서드 호출
             model.bothinfoTransfer();
             
             JOptionPane.showMessageDialog(view, "회원가입 성공!");
             view.dispose();
             
-            LoginView loginView = new LoginView();
-            LoginModel loginModel = new LoginModel();
-            new LoginController(loginView, loginModel);  // ⭐ Controller 연결해줘야 함
-
-            loginView.setVisible(true);  // 로그인 화면 띄우기
-            // new LoginView().setVisible(true);
+            SwingUtilities.invokeLater(() -> {
+                LoginView view = new LoginView();
+                LoginModel model = new LoginModel();
+                LoginController logincontroller = new LoginController(view, model);
+                view.setLocationRelativeTo(null);
+                view.setVisible(true);
+            });
         } else {
             JOptionPane.showMessageDialog(view, "회원가입 실패. 다시 시도하세요.");
         }
