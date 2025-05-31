2b5c0bc20aeadcd29486bc8805c277a98fa02377
myname-jin <adsd3@example.com>
2025-05-31 20:00:33 +0900

    fix : 회원가입
    소켓 연결

--- src/main/java/login/SignupController.java
+++ src/main/java/login/SignupController.java
@@ -17,6 +17,7 @@ import java.io.OutputStreamWriter;
 import java.net.Socket;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import ServerClient.SocketManager;
 
 public class SignupController {
     private final SignupView view;
@@ -55,7 +56,7 @@ public class SignupController {
         }
 
         // 서버 연결 & 메시지 전송
-        try (Socket socket = new Socket("127.0.0.1", 5000);
+        try (Socket socket = SocketManager.getSocket();
              BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
              BufferedReader in  = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
